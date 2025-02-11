d77db5e74f93f1f8a95a92eab822a615d5b94be5
Shubham Kumar <shubhamskkr87@gmail.com>
2025-02-11 15:12:06 +0530

    update planet service

--- test-and-taste-developer-exercise-main/Test-Taste-Console-Application/Domain/Services/PlanetService.cs
+++ test-and-taste-developer-exercise-main/Test-Taste-Console-Application/Domain/Services/PlanetService.cs
@@ -9,83 +9,57 @@ using Test_Taste_Console_Application.Domain.DataTransferObjects.JsonObjects;
 using Test_Taste_Console_Application.Domain.Objects;
 using Test_Taste_Console_Application.Domain.Services.Interfaces;
 using Test_Taste_Console_Application.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace Test_Taste_Console_Application.Domain.Services
 {
-    /// <inheritdoc />
-    public class PlanetService : IPlanetService
+
+public class PlanetService : IPlanetService
+{
+    private readonly HttpClient _httpClient;
+
+    public PlanetService(HttpClient httpClient)
     {
-        private readonly HttpClientService _httpClientService;
+        _httpClient = httpClient;
+    }
 
-        public PlanetService(HttpClientService httpClientService)
+    public async Task<List<Planet>> GetPlanetsAsync()
+    {
+        try
         {
-            _httpClientService = httpClientService;
+            HttpResponseMessage response = await _httpClient.GetAsync("planets");
+            response.EnsureSuccessStatusCode();
+
+            string json = await response.Content.ReadAsStringAsync();
+            var planetDtos = JsonSerializer.Deserialize<List<PlanetDto>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            return planetDtos?.ConvertAll(MapPlanetDtoToDomain) ?? new List<Planet>();
         }
-
-        public IEnumerable<Planet> GetAllPlanets()
+        catch (Exception ex)
         {
-            var allPlanetsWithTheirMoons = new Collection<Planet>();
-
-            var response = _httpClientService.Client
-                .GetAsync(UriPath.GetAllPlanetsWithMoonsQueryParameters)
-                .Result;
-
-            //If the status code isn't 200-299, then the function returns an empty collection.
-            if (!response.IsSuccessStatusCode)
-            {
-                Logger.Instance.Warn($"{LoggerMessage.GetRequestFailed}{response.StatusCode}");
-                return allPlanetsWithTheirMoons;
-            }
-
-            var content = response.Content.ReadAsStringAsync().Result;
-
-            //The JSON converter uses DTO's, that can be found in the DataTransferObjects folder, to deserialize the response content.
-            var results = JsonConvert.DeserializeObject<JsonResult<PlanetDto>>(content);
-
-            //The JSON converter can return a null object. 
-            if (results == null) return allPlanetsWithTheirMoons;
-
-            //If the planet doesn't have any moons, then it isn't added to the collection.
-            foreach (var planet in results.Bodies)
-            {
-                if(planet.Moons != null)
-                {
-                    var newMoonsCollection = new Collection<MoonDto>();
-                    foreach (var moon in planet.Moons)
-                    {
-                        var moonResponse = _httpClientService.Client
-                            .GetAsync(UriPath.GetMoonByIdQueryParameters + moon.URLId)
-                            .Result;
-                        var moonContent = moonResponse.Content.ReadAsStringAsync().Result;
-                        newMoonsCollection.Add(JsonConvert.DeserializeObject<MoonDto>(moonContent));
-                    }
-                    planet.Moons = newMoonsCollection;
-
-                }
-                allPlanetsWithTheirMoons.Add(new Planet(planet));
-            }
-
-            return allPlanetsWithTheirMoons;
-        }
-
-        private static string RemoveDiacritics(string text)
-        {
-            var normalizedString = text.Normalize(NormalizationForm.FormD);
-            var stringBuilder = new StringBuilder(capacity: normalizedString.Length);
-
-            for (int i = 0; i < normalizedString.Length; i++)
-            {
-                char c = normalizedString[i];
-                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
-                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
-                {
-                    stringBuilder.Append(c);
-                }
-            }
-
-            return stringBuilder
-                .ToString()
-                .Normalize(NormalizationForm.FormC);
+            Console.WriteLine($"Error fetching planets from API: {ex.Message}");
+            return new List<Planet>();
         }
     }
+
+    private Planet MapPlanetDtoToDomain(PlanetDto dto)
+    {
+        return new Planet
+        {
+            Name = dto.Name,
+            Moons = dto.Moons?.ConvertAll(m => new Moon
+            {
+                Name = m.Name,
+                Temperature = m.Temperature
+            }) ?? new List<Moon>()
+        };
+    }
+}
+
+
 }
