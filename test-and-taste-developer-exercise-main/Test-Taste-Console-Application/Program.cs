f6fca395341eb4b566631cfbf48fde0b91b9ace2
Shubham Kumar <shubhamskkr87@gmail.com>
2025-02-11 14:13:32 +0530

    adding api url

--- test-and-taste-developer-exercise-main/Test-Taste-Console-Application/Program.cs
+++ test-and-taste-developer-exercise-main/Test-Taste-Console-Application/Program.cs
@@ -8,54 +8,125 @@ using Test_Taste_Console_Application.Constants;
 using Test_Taste_Console_Application.Domain.Services;
 using Test_Taste_Console_Application.Domain.Services.Interfaces;
 using Test_Taste_Console_Application.Utilities;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Test_Taste_Console_Application
 {
-    class Program
+   using System;
+
+public class Moon
+{
+    public string Name { get; set; }
+    public double Mass { get; set; }
+    public double Temperature { get; set; }
+
+    public Moon(string name, double mass, double temperature)
     {
-        static void Main(string[] args)
+        Name = name;
+        Mass = mass;
+        Temperature = temperature;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} - Mass: {Mass}kg, Temperature: {Temperature}°C";
+    }
+}
+
+public class Planet
+{
+    public string Name { get; set; }
+    public double Mass { get; set; }
+    public List<Moon> Moons { get; set; }
+
+    public Planet(string name, double mass)
+    {
+        Name = name;
+        Mass = mass;
+        Moons = new List<Moon>();  
+    }
+
+    public void AddMoon(Moon moon)
+    {
+        Moons.Add(moon);
+    }
+
+    public double? AverageTemperatureOfMoons()
+    {
+        if (Moons.Count == 0)
         {
-            var serviceCollection = new ServiceCollection();
-            //The ConfigureServices function configures the services.
-            ConfigureServices(serviceCollection);
-            
-            //The RunServiceOperations function executes the code that can create the outputs.
-            RunServiceOperations(serviceCollection);
+            return null;  
         }
 
-        private static void RunServiceOperations(IServiceCollection serviceCollection)
+        double totalTemp = 0;
+        foreach (var moon in Moons)
         {
-            var serviceProvider = serviceCollection.BuildServiceProvider();
-
-            //The service provider gets the services.
-            var screenOutputService = serviceProvider.GetService<IOutputService>();
-
-            try
-            {
-                screenOutputService.OutputAllPlanetsAndTheirAverageMoonGravityToConsole();
-                screenOutputService.OutputAllMoonsAndTheirMassToConsole();
-                screenOutputService.OutputAllPlanetsAndTheirMoonsToConsole();
-            }
-            catch (Exception exception)
-            {
-                //The users and developers can see the thrown exceptions.
-                Logger.Instance.Error($"{LoggerMessage.ScreenOutputOperationFailed}{exception.Message}");
-                Console.WriteLine($"{ExceptionMessage.ScreenOutputOperationFailed}{exception.Message}");
-                System.Diagnostics.Debug.WriteLine($""{ExceptionMessage.ScreenOutputOperationFailed}{exception.Message}"");
-            }
-
-            serviceProvider.Dispose();
+            totalTemp += moon.Temperature;
         }
 
-        private static void ConfigureServices(IServiceCollection serviceCollection)
+        return totalTemp / Moons.Count;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} - Mass: {Mass}kg";
+    }
+}
+
+public class Program
+{
+    
+    public static async Task<List<Planet>> FetchPlanetsAsync(string apiUrl)
+    {
+        using (var client = new HttpClient())
         {
-            //The function configures all the services.
-            XmlConfigurator.Configure(LogManager.GetRepository(Assembly.GetEntryAssembly()),
-                new FileInfo(ConfigurationFileName.Logger));
-            serviceCollection.AddHttpClient<HttpClientService>();
-            serviceCollection.AddSingleton<IPlanetService, PlanetService>();
-            serviceCollection.AddSingleton<IOutputService, ScreenOutputService>();
-            serviceCollection.AddSingleton<IMoonService, MoonService>();
+            var response = await client.GetStringAsync(apiUrl);
+            var planets = JsonConvert.DeserializeObject<List<Planet>>(response);
+            return planets;
+        }
+    }
+
+    public static List<(Planet, double?)> ListPlanetsWithMoonsAndAvgTemperature(List<Planet> planets)
+    {
+        var planetsWithMoons = new List<(Planet, double?)>();
+
+        foreach (var planet in planets)
+        {
+            if (planet.Moons.Count > 0) 
+            {
+                double? avgTemp = planet.AverageTemperatureOfMoons();
+                planetsWithMoons.Add((planet, avgTemp));
+            }
+        }
+
+        return planetsWithMoons;
+    }
+
+    public static async Task Main(string[] args)
+    {
+        string apiUrl = "https://api.le-systeme-solaire.net";
+
+        try
+        {
+      
+            var planets = await FetchPlanetsAsync(apiUrl);
+
+            var planetsWithAvgTemp = ListPlanetsWithMoonsAndAvgTemperature(planets);
+
+            foreach (var entry in planetsWithAvgTemp)
+            {
+                var planet = entry.Item1;
+                var avgTemp = entry.Item2;
+                Console.WriteLine($"{planet.Name} has an average moon temperature of {avgTemp}°C");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error fetching data: {ex.Message}");
         }
     }
 }
+
